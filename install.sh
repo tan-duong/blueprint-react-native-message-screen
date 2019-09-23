f1e91b4a38621d1ccc14f7b992b37c56e4312bc9
tanduong <tanduong@live.com>
2019-09-23 18:15:41 -0500

    Use $BASE_PATH instead of $APP_NAME in paths

--- install.sh
+++ install.sh
@@ -26,17 +26,17 @@ DATA_2="import { ${BLUEPRINT}Navigator } from '..\/features\/${BLUEPRINT}\/navig
 DATA_3="${BLUEPRINT}: { screen: ${BLUEPRINT}Navigator },"
 
 echo "create blueprint folder"
-mkdir -p $APP_NAME/src/features/$BLUEPRINT
+mkdir -p $BASE_PATH/src/features/$BLUEPRINT
 
 echo "copy"
-cp -r ./$BLUEPRINT/. $APP_NAME/src/features/$BLUEPRINT
+cp -r ./$BLUEPRINT/. $BASE_PATH/src/features/$BLUEPRINT
 
 echo ">> insert 1" 
-sed -i "s/${EXT_POINT_1}/&\n${DATA_1}/g" $APP_NAME/src/config/installed_blueprints.js
+sed -i "s/${EXT_POINT_1}/&\n${DATA_1}/g" $BASE_PATH/src/config/installed_blueprints.js
 
 
 echo ">> insert 2"
-sed -i "s/${EXT_POINT_2}/&\n${DATA_2}/g" $APP_NAME/src/navigator/mainNavigator.js
+sed -i "s/${EXT_POINT_2}/&\n${DATA_2}/g" $BASE_PATH/src/navigator/mainNavigator.js
 
 echo ">> insert 3"
-sed -i "s/${EXT_POINT_3}/&\n${DATA_3}/g" $APP_NAME/src/navigator/mainNavigator.js
+sed -i "s/${EXT_POINT_3}/&\n${DATA_3}/g" $BASE_PATH/src/navigator/mainNavigator.js
